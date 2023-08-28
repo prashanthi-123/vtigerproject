4d6f2ce06e512ac788df4c62671796e1f3771433
prashanthi-123 <139227940+prashanthi-123@users.noreply.github.com>
2023-08-28 09:17:15 +0530

    Update CreateAndDuplicateLeadTest.java

--- src/test/java/hardcodedtest/CreateAndDuplicateLeadTest.java
+++ src/test/java/hardcodedtest/CreateAndDuplicateLeadTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//This Lead is Created And Duplicate program 
 public class CreateAndDuplicateLeadTest {
 
 	public static void main(String[] args) throws InterruptedException {
