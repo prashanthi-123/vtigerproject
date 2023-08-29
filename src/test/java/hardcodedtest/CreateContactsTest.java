404b5cb16fcfc90909750532d81eb903bd41d3b2
prashanthi-123 <139227940+prashanthi-123@users.noreply.github.com>
2023-08-29 18:37:31 +0530

    Update CreateContactsTest.java

--- src/test/java/hardcodedtest/CreateContactsTest.java
+++ src/test/java/hardcodedtest/CreateContactsTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//CONTACT
 public class CreateContactsTest {
 
 	public static void main(String[] args) throws InterruptedException {
